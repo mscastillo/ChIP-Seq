a634b2841e233e53e46d26dee2b04a1911c556a5
Manuel Sánchez Castillo <sanchez.castillo@gmail.com>
2014-10-29 17:35:07 +0000

    First commit
    This script will compute the Fraction of Reads-in-Peaks (FRiP, see PubMed 22955991) for all peaks file in the current folder. It requires to be have the original file with the aligned reads, in BED format, into the same folder.

---
+++ frip.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+FILES=($(ls *400bp.bed))
+BED_FILE=$(ls *.BED)
+
+for k in $( seq 0 $((${#FILES[@]} - 1)) ) ; do
+
+ FILE=${FILES[$k]}
+ NAME="${FILE%.*}"
+
+ if [[ -f $BED_FILE && -f $NAME.bed ]] ; then
+
+ intersectBed -a $NAME.bed -b $BED_FILE -wa > $NAME.intersect
+ mergeBed -i $NAME.intersect -n > $NAME.counts
+ READS=$(cat $NAME.intersect | wc -l )
+ RiP=$(cat $NAME.counts | wc -l )
+ FRiP=$( bc -l <<< " scale=2 ; 100*PEAKS/RiP  " )
+ echo $NAME$'\t'$FRiP
+ 
+ rm -f $NAME.intersect
+ rm -f $NAME.counts
+ 
+ fi
+
+done
