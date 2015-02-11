cce159ca55e8832040137ad23ec6883043a14503
Manuel Sánchez Castillo <sanchez.castillo@gmail.com>
2015-02-11 18:20:24 +0000

    Update bw2histogram.sh

--- bw2histogram.sh
+++ bw2histogram.sh
@@ -9,12 +9,12 @@
 
 ## VARIABLES # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-   PEAKS_FILE='peaks.bed' # A peaks file, in bed format, with the genomic coordinates to where it is desired to count the histones. Minimun five columns (additional details, http://homer.salk.edu/homer/ngs/quantification.html).
+   PEAKS_FILE='peaks.bed' # A file with the genomic coordinates to where it is desired to compute the histogram. Minimun five columns (additional details, http://homer.salk.edu/homer/ngs/quantification.html).
    GENOME='mm10' # Genome considered for the alignment. Possible values: 'hg19', 'mm10', etc...
    
 ## PARAMETERS # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-   BED_FILES=($(ls *.BED)) # Will perform the analysis on all the BED files in the current folder. It requires to have one bigwig file for each BED.
+   bw_FILES=($(ls *.bw)) # Will perform the analysis on all the bw files of current folder.
    REGION_SIZE=2000 # Region arround the peak centre (in bp) where to perform the analysis.
    BIN_SIZE=10 # Size of the bin (in bp) to perform the analysis.
    PATH=$PATH:/home/Programs/HOMER/bin/:/home/Programs/UCSC_programs/ # Update the PATH with the corresponding dependencies.
@@ -26,7 +26,6 @@ for k in $( seq 0 $((${#BED_FILES[@]} - 1)) ) ; do
    FILE=${BED_FILES[$k]}
    NAME=${FILE%.*}
    OUTPUTFILE=$NAME".matrix"
-#  wget http://database.com/$GENOME/$NAME.bw # optional step if the bigwig files are not locally stored
    bigWigToWig $NAME.bw $NAME.bedGraph
    annotatePeaks.pl $PEAKS_FILE $GENOME -size $REGION_SIZE -hist $BIN_SIZE -bedGraph $NAME.bedGraph -pcount -ghist -strand both -noadj > $OUTPUTFILE
    rm -f $NAME.bedGraph
